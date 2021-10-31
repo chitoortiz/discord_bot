9950bc754571a33103959db7b9169d7b0d73f381
chitoortiz <ortizcanavatenacho17@gmail.com>
2021-10-31 18:01:49 +0100

    First Commit
    Added secretsanta.py

---
+++ secretsanta.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author.id == "264423375542157312":
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('ODU3MTgxNDM1NjI2Mzg5NTQ1.YNL2aw.V_adMHoTEeqXImxEg5vmfSF9vjE')
