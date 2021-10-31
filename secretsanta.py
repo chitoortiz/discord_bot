6c2487fa0ae6ec9c1b6dda59c4febc47f7a08b70
chitoortiz <ortizcanavatenacho17@gmail.com>
2021-10-31 19:09:05 +0100

    Final version without all players
    Sends message to player for who he has to gift.

--- secretsanta.py
+++ secretsanta.py
@@ -5,16 +5,20 @@ from randomise import *
 
 client = discord.Client()
 
+idNacho = 264423375542157312
+idSofía = 881858600929198110
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author.id == "264423375542157312":
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('#quien'): 
+        if message.author.id == idNacho:
+            await message.author.send(Nacho)
+        if message.author.id == idSofía:
+            await message.author.send(Sofía)
+
 
 client.run('ODU3MTgxNDM1NjI2Mzg5NTQ1.YNL2aw.V_adMHoTEeqXImxEg5vmfSF9vjE')
